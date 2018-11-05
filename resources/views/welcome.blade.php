7d2c69a4c5d26ee8b27b51605f6189f63d46d644
ZouFoo <1970226903@qq.com>
2018-11-05 11:50:03 +0800

    change welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello world!
+                    Welcome
                 </div>
 
                 {{--<div class="links">--}}
